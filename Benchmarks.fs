aa60d9f7d48f082008abf9d3c24db1a3190a3779
Humza Shahid <humzasaur@gmail.com>
2022-12-03 18:03:01 +0000

    add intmap

--- Benchmarks.fs
+++ Benchmarks.fs
@@ -14,6 +14,10 @@ module InsertData =
     let emptyTree = RedBlackTree.empty
     let mutable tree = emptyTree
 
+    (* IntMap data for IterationSetup *)
+    let emptyIntMap = IntMap.empty
+    let mutable intMap = emptyIntMap
+
     (* Random number data for insertion tests *)
     let mutable rnd = System.Random()
     let mutable randomInsNum = 0
@@ -21,45 +25,81 @@ module InsertData =
     let afterInsNum = 9000_000 (* This number is larger than all the numbers already in the structure. *)
 
 [<MemoryDiagnoser; HtmlExporter; MarkdownExporter>]
-type Insert () =
+type ListZipperBenchmarks () =
     [<Params(100, 1000, 10_000, 100_000, 1000_000)>]
     member val public structureSize = 0 with get, set
 
     [<IterationSetup>]
     member this.createWithSize() =
         InsertData.zipper <- InsertData.emptyZipper
-        InsertData.tree <- InsertData.emptyTree
         InsertData.randomInsNum <- InsertData.rnd.Next(0, this.structureSize)
         for i in [0..this.structureSize] do
             InsertData.zipper <- ListZipper.insert i InsertData.zipper
-            InsertData.tree <- RedBlackTree.insert i InsertData.tree
 
     [<Benchmark(Description = "ListZipper.insert at start"); IterationCount 10>]
     member this.ListZipperInsertAtStart() =
         ListZipper.insert InsertData.beforeInsNum InsertData.zipper
 
-    [<Benchmark(Description = "RbTree.insert at start"); IterationCount 10>]
-    member this.RbTreeInsertAtStart() =
-        RedBlackTree.insert InsertData.beforeInsNum InsertData.tree
-
     [<Benchmark(Description = "Random ListZipper.insert"); IterationCount 10>]
     member this.RandomListZipperInsert() =
         ListZipper.insert InsertData.randomInsNum InsertData.zipper
-
-    [<Benchmark(Description = "Random RbTree.insert"); IterationCount 10>]
-    member this.RandomRbTreeInsert() =
-        RedBlackTree.insert InsertData.randomInsNum InsertData.tree
 
     [<Benchmark(Description = "ListZipper.insert at end"); IterationCount 10>]
     member this.ListZipperInsertAtEnd() =
         ListZipper.insert InsertData.afterInsNum InsertData.zipper
 
+[<MemoryDiagnoser; HtmlExporter; MarkdownExporter>]
+type RbTreeBenchmarks() =
+    [<Params(100, 1000, 10_000, 100_000, 1000_000)>]
+    member val public structureSize = 0 with get, set
+
+    [<IterationSetup>]
+    member this.createWithSize() =
+        InsertData.tree <- InsertData.emptyTree
+        InsertData.randomInsNum <- InsertData.rnd.Next(0, this.structureSize)
+        for i in [0..this.structureSize] do
+            InsertData.tree <- RedBlackTree.insert i InsertData.tree
+
+    [<Benchmark(Description = "RbTree.insert at start"); IterationCount 10>]
+    member this.RbTreeInsertAtStart() =
+        RedBlackTree.insert InsertData.beforeInsNum InsertData.tree
+
+    [<Benchmark(Description = "Random RbTree.insert"); IterationCount 10>]
+    member this.RandomRbTreeInsert() =
+        RedBlackTree.insert InsertData.randomInsNum InsertData.tree
+
     [<Benchmark(Description = "RbTree.insert at end"); IterationCount 10>]
     member this.RbTreeInsertAtEnd() =
         RedBlackTree.insert InsertData.afterInsNum InsertData.tree
 
+[<MemoryDiagnoser; HtmlExporter; MarkdownExporter>]
+type IntMapBenchmarks() =
+    [<Params(100, 1000, 10_000, 100_000, 1000_000)>]
+    member val public structureSize = 0 with get, set
+
+    [<IterationSetup>]
+    member this.createWithSize() =
+        InsertData.intMap <- InsertData.emptyIntMap
+        InsertData.randomInsNum <- InsertData.rnd.Next(0, this.structureSize)
+        for i in [0..this.structureSize] do
+            InsertData.intMap <- IntMap.insert i InsertData.intMap
+
+    [<Benchmark(Description = "IntMap.insert at start"); IterationCount 10>]
+    member this.RbTreeInsertAtStart() =
+        IntMap.insert InsertData.beforeInsNum InsertData.intMap
+
+    [<Benchmark(Description = "Random IntMap.insert"); IterationCount 10>]
+    member this.RandomRbTreeInsert() =
+        IntMap.insert InsertData.randomInsNum InsertData.intMap
+
+    [<Benchmark(Description = "IntMap.insert at end"); IterationCount 10>]
+    member this.RbTreeInsertAtEnd() =
+        IntMap.insert InsertData.afterInsNum InsertData.intMap
+
 module Main = 
     [<EntryPoint>]
     let Main _ =
-        BenchmarkRunner.Run<Insert>() |> ignore
+        BenchmarkRunner.Run<ListZipperBenchmarks>() |> ignore
+        BenchmarkRunner.Run<RbTreeBenchmarks>() |> ignore
+        BenchmarkRunner.Run<IntMapBenchmarks>() |> ignore
         0
---
+++ IntMap.fs
@@ -0,0 +1,51 @@
+﻿namespace ListZipperVsRbTree
+
+(* Copied from: https://github.com/fsprojects/FSharpx.Collections/blob/master/src/FSharpx.Collections.Experimental/IntMap.fs *)
+
+module IntMap =
+    type IntMap =
+        | Nil
+        | Tip of int 
+        | Bin of int * int * IntMap * IntMap
+
+    ///O(1). The empty map. Credit: Haskell.org
+    let empty = Nil
+        
+    let inline private maskW i m =
+        int(i &&& (~~~(m - 1ul) ^^^ m))
+
+    let inline private mask i m =
+        maskW (uint32 i) (uint32 m)
+
+    let inline private nomatch i p m =
+        mask i m <> p
+
+    let inline private zero i m =
+        (uint32 i) &&& (uint32 m) = 0ul
+
+    let inline private highestBitMask x0 =
+        let x1 = x0 ||| (x0 >>> 1)
+        let x2 = x1 ||| (x1 >>> 2)
+        let x3 = x2 ||| (x2 >>> 4)
+        let x4 = x3 ||| (x3 >>> 8)
+        let x5 = x4 ||| (x4 >>> 16)
+        let x6 = x5 ||| (x5 >>> 32) // for 64 bit platforms
+        x6 ^^^ (x6 >>> 1)
+
+    let inline private branchMask p1 p2 =
+        int(highestBitMask(uint32 p1 ^^^ uint32 p2))
+
+    let inline private join p1 t1 p2 t2 =
+        let m = branchMask p1 p2
+        let p = mask p1 m
+        if zero p1 m then Bin(p, m, t1, t2) else Bin(p, m, t2, t1)
+
+    ///O(min(n,W)). Insert a new key/value pair in the map. If the key is already present in the map, the associated value is replaced with the supplied value, i.e. insert is equivalent to insertWith const. Credit: Haskell.org
+    let rec insert k t =
+        match t with
+        | Bin(p, m, l, r) when nomatch k p m -> join k (Tip(k)) p t
+        | Bin(p, m, l, r) when zero k m -> Bin(p, m, insert k l, r)
+        | Bin(p, m, l, r) -> Bin(p, m, l, insert k r)
+        | Tip(ky) when k = ky -> Tip(k)
+        | Tip(ky) -> join k (Tip(k)) ky t
+        | Nil -> Tip(k)
